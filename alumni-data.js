7a96457fdc536c99b065de7a32a4cedf7281c52e
keerthana2624 <keerthanamummy2002@gmail.com>
2025-06-12 09:06:55 +0530

    created alumni data file to store the details

---
+++ alumni-data.js
@@ -0,0 +1,56 @@
+// Alumni Registration Details Data
+const alumniData = [
+    {
+        slNo: 1,
+        rollNumber: "2023501058",
+        name: "K.Keerthana",
+        degree: "Ph.D",
+        type: "Part Time",
+        email: "keerthikeerthana@gmail.com"
+    },
+    {
+        slNo: 2,
+        rollNumber: "2023501053",
+        name: "R.Kavya Sri",
+        degree: "M.Tech",
+        type: "Full Time",
+        email: "kavyaramagiri@gmail.com"
+    },
+    {
+        slNo: 3,
+        rollNumber: "2023501057",
+        name: "V.Sriram",
+        degree: "B.Tech",
+        type: "Full Time",
+        email: "sriramvarma@gmail.com"
+    },
+    {
+        slNo: 4,
+        rollNumber: "2023501053",
+        name: "K.Krishna",
+        degree: "B.Tech",
+        type: "Part Time",
+        email: "krishnaprasad@gmail.com"
+    },
+    {
+        slNo: 5,
+        rollNumber: "2023501062",
+        name: "P.Rakesh",
+        degree: "Ph.D",
+        type: "Part Time",
+        email: "rakeshpodishetti@gmail.com"
+    },
+    {
+        slNo: 6,
+        rollNumber: "2023501078",
+        name: "K.Sushmitha",
+        degree: "M.Tech",
+        type: "Part Time",
+        email: "Sushmitha@gmail.com"
+    }
+];
+
+// Export the data for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = alumniData;
+} 
